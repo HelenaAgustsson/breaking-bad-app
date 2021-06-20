c0524477276a794e16e98a0ea4e074764510e0a0
Helena Agustsson <you@example.com>
2021-06-20 15:19:50 +0100

    header component, axios

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-class MyComponentClass extends React.Component {
-  render() {
-    return <h1>Hello</h1>;
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponentClass />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
